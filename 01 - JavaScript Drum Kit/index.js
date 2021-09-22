521b8b0c2eb98592ceb5f6fcc9eae72cde3ec2a4
Mert Arslan <merarslan9@gmail.com>
2021-09-22 14:38:49 +0300

    day 1 completed

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,7 @@
+window.addEventListener('keydown', function (e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  console.log(audio);
+  if (!audio) return; //stop the function from running at all
+  audio.currentTime(0);
+  audio.play();
+});
